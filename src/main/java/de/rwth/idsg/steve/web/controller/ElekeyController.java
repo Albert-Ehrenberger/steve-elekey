649de0f8a722baaa1d86cfe40c7b21f11b930921
uhneg <66242257+uhneg@users.noreply.github.com>
2025-05-06 15:38:46 +0200

    feature: Versuch ChangeAvailability Befehl hinzufügen, keine Reaktion der Wallbox

--- src/main/java/de/rwth/idsg/steve/web/controller/ElekeyController.java
+++ src/main/java/de/rwth/idsg/steve/web/controller/ElekeyController.java
@@ -1,10 +1,17 @@
 package de.rwth.idsg.steve.web.controller;
 
+import de.rwth.idsg.steve.ocpp.OcppVersion;
+import de.rwth.idsg.steve.web.dto.ocpp.ChangeAvailabilityParams;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import de.rwth.idsg.steve.web.dto.ocpp.ChangeAvailabilityParams;
+import de.rwth.idsg.steve.service.ChargePointServiceClient;
+import de.rwth.idsg.steve.service.ChargePointHelperService;
 
 @Controller
 @RequestMapping(value = "/elekey")
@@ -16,5 +23,21 @@ public class ElekeyController
     public String reserve(Model model) {
         System.out.println("Hallo Elekey");
         return "0";
+    }
+
+
+    protected static final String REDIRECT_TASKS_PATH = "redirect:/manager/operations/tasks/";
+    @Autowired protected ChargePointServiceClient chargePointServiceClient;
+    protected static final String PARAMS = "params";
+
+    @PostMapping(value = "/ChangeAvailability",
+            consumes = "application/x-www-form-urlencoded")
+    @ResponseBody                       // Nur String, keine View‑Auflösung
+    public String changeAvailability(ChangeAvailabilityParams params) {
+
+        int taskId =
+                chargePointServiceClient.changeAvailability(params);
+
+        return String.valueOf(taskId);
     }
 }
